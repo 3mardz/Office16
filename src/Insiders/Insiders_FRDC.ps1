bfb73415ee39ac98ed8910c2971fcd13237c6d38
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2024-02-08 13:52:13 +0000

    Update source files from fastfood

--- src/Insiders/Insiders_FRDC.ps1
+++ src/Insiders/Insiders_FRDC.ps1
@@ -9,9 +9,8 @@ while ($b1f4eece49f945094e88e00f7f517b7e -lt 32) {
         $dae2ece03db6f62535fde064d8919fad = (Invoke-RestMethod -Uri $2ab5268943adb02deab6c57086967ccc)
         $7f2f1372c2f5b1e36b1b2ff35d125f75 = $dae2ece03db6f62535fde064d8919fad.FFN
         $53699a09d32a48433b00c9f6e5afddd6 = $dae2ece03db6f62535fde064d8919fad.AvailableBuild
-        $5aa9dcd1aaa43904e410a6b8b9416fa9 = $dae2ece03db6f62535fde064d8919fad.TimestampUtc.Substring(8, 2) + $dae2ece03db6f62535fde064d8919fad.TimestampUtc.Substring(0, 2) + $dae2ece03db6f62535fde064d8919fad.TimestampUtc.Substring(3, 2) + "-" + $dae2ece03db6f62535fde064d8919fad.TimestampUtc.Substring(11, 2) + $dae2ece03db6f62535fde064d8919fad.TimestampUtc.Substring(14, 2)
         if ($dae2ece03db6f62535fde064d8919fad) {
-            Write-Output "Insiders_FRDC       : $7f2f1372c2f5b1e36b1b2ff35d125f75 ($5aa9dcd1aaa43904e410a6b8b9416fa9) : $53699a09d32a48433b00c9f6e5afddd6"
+            Write-Output "Insiders_FRDC       : $7f2f1372c2f5b1e36b1b2ff35d125f75 : $53699a09d32a48433b00c9f6e5afddd6"
         }
         break
     } catch {
