689887044ce83e187e29f0df9d759bacd09d5c9f
Levvie - she/her <11600822+ItzLevvie@users.noreply.github.com>
2024-04-06 20:29:28 +0100

    Update to the latest source code files

---
+++ src/Insiders/Insiders_LTSC.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = "SilentlyContinue"
+$ProgressPreference = "SilentlyContinue"
+
+$2ab5268943adb02deab6c57086967ccc = "https://mrodevicemgr.officeapps.live.com/mrodevicemgrsvc/api/v2/C2RReleaseData/2e148de9-61c8-4051-b103-4af54baffbb4"
+
+$b1f4eece49f945094e88e00f7f517b7e = 0
+while ($b1f4eece49f945094e88e00f7f517b7e -lt 32) {
+    try {
+        $dae2ece03db6f62535fde064d8919fad = (Invoke-RestMethod -Uri $2ab5268943adb02deab6c57086967ccc)
+        $7f2f1372c2f5b1e36b1b2ff35d125f75 = $dae2ece03db6f62535fde064d8919fad.FFN
+        $53699a09d32a48433b00c9f6e5afddd6 = $dae2ece03db6f62535fde064d8919fad.AvailableBuild
+        if ($dae2ece03db6f62535fde064d8919fad) {
+            Write-Output "Insiders_LTSC       : $7f2f1372c2f5b1e36b1b2ff35d125f75 : $53699a09d32a48433b00c9f6e5afddd6"
+        }
+        break
+    } catch {
+        $b1f4eece49f945094e88e00f7f517b7e++
+    }
+}
+
+Remove-Variable * -ErrorAction SilentlyContinue
---
+++ src/Production/Production_LTSC2024.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = "SilentlyContinue"
+$ProgressPreference = "SilentlyContinue"
+
+$2ab5268943adb02deab6c57086967ccc = "https://mrodevicemgr.officeapps.live.com/mrodevicemgrsvc/api/v2/C2RReleaseData/7983bac0-e531-40cf-be00-fd24fe66619c"
+
+$b1f4eece49f945094e88e00f7f517b7e = 0
+while ($b1f4eece49f945094e88e00f7f517b7e -lt 32) {
+    try {
+        $dae2ece03db6f62535fde064d8919fad = (Invoke-RestMethod -Uri $2ab5268943adb02deab6c57086967ccc)
+        $7f2f1372c2f5b1e36b1b2ff35d125f75 = $dae2ece03db6f62535fde064d8919fad.FFN
+        $53699a09d32a48433b00c9f6e5afddd6 = $dae2ece03db6f62535fde064d8919fad.AvailableBuild
+        if ($dae2ece03db6f62535fde064d8919fad) {
+            Write-Output "Production_LTSC2024 : $7f2f1372c2f5b1e36b1b2ff35d125f75 : $53699a09d32a48433b00c9f6e5afddd6"
+        }
+        break
+    } catch {
+        $b1f4eece49f945094e88e00f7f517b7e++
+    }
+}
+
+Remove-Variable * -ErrorAction SilentlyContinue
